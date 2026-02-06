91b8b3e384348dcbe4fb9c4bb0f185f76d1a7ccc
Rustam Sadreev <rustam17.sb@gmail.com>
2026-02-06 03:04:31 +0300

    feat(presenter): add catalog presenter

---
+++ src/const.js
@@ -0,0 +1,3 @@
+const MOVIES_PER_LOAD = 5;
+
+export { MOVIES_PER_LOAD };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { render } from './framework/render.js';
 import UserView from './view/user-view.js';
 import MoviesCountView from './view/movies-count-view.js';
+import CatalogPresenter from './presenter/catalog-presenter.js';
 
 const siteHeaderContainerElement = document.body.querySelector('.site-header__container');
 const siteFooterContainerElement = document.body.querySelector('.site-footer__container');
+const catalogContainerElement = document.body.querySelector('.catalog__container');
+
+const catalogPresenter = new CatalogPresenter({ catalogContainerElement });
 
 render(new UserView(), siteHeaderContainerElement);
 render(new MoviesCountView(), siteFooterContainerElement);
+
+catalogPresenter.init();
