b7cd4d9d5df051ba848b5035503f3acbc42cf179
Rustam Sadreev <rustam17.sb@gmail.com>
2026-02-06 03:04:31 +0300

    feat(presenter): add catalog presenter

---
+++ src/presenter/catalog-presenter.js
@@ -0,0 +1,27 @@
+import { render } from '../framework';
+import { MOVIES_PER_LOAD } from '../const.js';
+import CatalogFilterView from '../view/catalog-filter-view.js';
+import CatalogSortingView from '../view/catalog-sorting-view.js';
+import CatalogListView from '../view/catalog-list-view.js';
+import MovieCardView from '../view/movie-card-view.js';
+import CatalogShowMoreButtonView from '../view/catalog-show-more-button-view.js';
+
+export default class CatalogPresenter {
+  catalogListComponent = new CatalogListView();
+
+  constructor({ catalogContainerElement }) {
+    this.catalogContainerElement = catalogContainerElement;
+  }
+
+  init() {
+    render(new CatalogFilterView(), this.catalogContainerElement);
+    render(new CatalogSortingView(), this.catalogContainerElement);
+    render(this.catalogListComponent, this.catalogContainerElement);
+
+    for (let i = 0; i < MOVIES_PER_LOAD; i++) {
+      render(new MovieCardView(), this.catalogListComponent.getElement());
+    }
+
+    render(new CatalogShowMoreButtonView(), this.catalogContainerElement);
+  }
+}
