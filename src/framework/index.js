09c2ad10c1cc7344beb2e600dd80db20aae90a65
Rustam Sadreev <rustam17.sb@gmail.com>
2026-02-27 00:45:39 +0300

    feat(framework): add AbstractView base class

--- src/framework/index.js
+++ src/framework/index.js
@@ -1,1 +1,2 @@
 export { RenderPosition, createElement, render } from './render.js';
+export { default as AbstractView } from './view/abstract-view.js';
---
+++ src/framework/view/abstract-view.js
@@ -0,0 +1,33 @@
+import { createElement } from '../render.js';
+
+/**
+ * Абстрактный класс представления
+ */
+export default class AbstractView {
+  /** @type {HTMLElement|null} Элемент представления */
+  #element = null;
+
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error('AbstractView can\'t be instantiated directly');
+    }
+  }
+
+  /**
+   * Геттер для получения элемента
+   * @returns {HTMLElement} Элемент представления
+   */
+  get element() {
+    this.#element ??= createElement(this._getTemplate());
+    return this.#element;
+  }
+
+  /**
+   * Метод для получения разметки элемента
+   * @abstract
+   * @returns {string} Разметка элемента в виде строки
+   */
+  _getTemplate() {
+    throw new Error('Abstract method not implemented: _getTemplate');
+  }
+}
