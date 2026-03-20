e87b38276a04d06c7ef46ca5d920a6c1671ef11c
Rustam Sadreev <rustam17.sb@gmail.com>
2026-03-21 00:37:52 +0300

    refactor(view): generate catalog sort items from config

--- src/view/catalog-sort-view.js
+++ src/view/catalog-sort-view.js
@@ -1,27 +1,33 @@
 import { AbstractView } from '../framework';
 import { SortType } from '../constants.js';
 
-function createCatalogSortTemplate() {
+const items = [
+  { label: 'default', value: SortType.DEFAULT },
+  { label: 'date', value: SortType.DATE_DESC },
+  { label: 'rating', value: SortType.RATING_DESC },
+];
+
+function createCatalogSortTemplate(currentValue) {
   return (
     `<form class="catalog__sort catalog-sort" action="https://echo.htmlacademy.ru/courses" method="get">
-      <label class="checker checker--simple checker--size_m">
-        <input class="checker__control visually-hidden" name="type" value="${SortType.DEFAULT}" type="radio" checked>
-        <span class="checker__label">Sort by default</span>
-      </label>
-      <label class="checker checker--simple checker--size_m">
-        <input class="checker__control visually-hidden" name="type" value="${SortType.DATE_DESC}" type="radio">
-        <span class="checker__label">Sort by date</span>
-      </label>
-      <label class="checker checker--simple checker--size_m">
-        <input class="checker__control visually-hidden" name="type" value="${SortType.RATING_DESC}" type="radio">
-        <span class="checker__label">Sort by rating</span>
-      </label>
+      ${items.map(({ label, value }) => `
+        <label class="checker checker--simple checker--size_m">
+          <input
+            class="checker__control visually-hidden"
+            name="type"
+            value="${value}"
+            type="radio"
+            ${value === currentValue ? 'checked' : ''}
+          >
+          <span class="checker__label">Sort by ${label}</span>
+        </label>
+      `).join('')}
     </form>`
   );
 }
 
 export default class CatalogSortView extends AbstractView {
   _getTemplate() {
-    return createCatalogSortTemplate();
+    return createCatalogSortTemplate(SortType.DEFAULT);
   }
 }
