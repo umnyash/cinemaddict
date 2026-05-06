cbfe00be7214d9f388acadcb12d14ffbba1b7830
Rustam Sadreev <rustam17.sb@gmail.com>
2026-05-06 22:00:50 +0300

    feat: render user rank using MoviesModel data

--- src/presenter/user-presenter.js
+++ src/presenter/user-presenter.js
@@ -1,14 +1,49 @@
-import { render } from '../framework';
+import { render, replace } from '../framework';
+import { EventType } from '../constants.js';
+import { determineUserRank } from '../utils';
 import UserView from '../view/user-view.js';
 
 export default class UserPresenter {
   #containerElement = null;
+  #moviesModel = null;
+  #userComponent = null;
+  #rank = null;
 
-  constructor({ containerElement }) {
+  constructor({ containerElement, moviesModel }) {
     this.#containerElement = containerElement;
+    this.#moviesModel = moviesModel;
+
+    this.#moviesModel.addObserver(this.#moviesModelEventHandler);
+  }
+
+  get #watchedMoviesCount() {
+    return this.#moviesModel.movies
+      .filter(({ isWatched }) => isWatched)
+      .length;
   }
 
   init() {
-    render(new UserView(), this.#containerElement);
+    const newRank = determineUserRank(this.#watchedMoviesCount);
+
+    if (newRank && newRank === this.#rank) {
+      return;
+    }
+
+    const newUserComponent = new UserView({ rank: newRank });
+
+    if (this.#userComponent) {
+      replace(newUserComponent, this.#userComponent);
+    } else {
+      render(newUserComponent, this.#containerElement);
+    }
+
+    this.#userComponent = newUserComponent;
+    this.#rank = newRank;
   }
+
+  #moviesModelEventHandler = (eventType) => {
+    if (eventType === EventType.MOVIE_WATCHED_TOGGLE) {
+      this.init();
+    }
+  };
 }
