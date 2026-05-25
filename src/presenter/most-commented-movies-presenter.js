eb1456d0b58ea91120ff871d82e27c6936aae1e1
Rustam Sadreev <rustam17.sb@gmail.com>
2026-05-25 22:36:15 +0300

    feat(presenter): add MostCommentedMoviesPresenter

---
+++ src/presenter/most-commented-movies-presenter.js
@@ -0,0 +1,97 @@
+import { render, remove } from '../framework';
+import { EventType } from '../constants.js';
+import { getTopMovies } from '../utils';
+
+import MovieCardPresenter from './movie-card-presenter.js';
+import MostCommentedMoviesView from '../view/most-commented-movies-view.js';
+import MovieListView from '../view/movie-list-view.js';
+
+const TOP_MOVIES_LIMIT = 2;
+
+export default class TopRatedMoviesPresenter {
+  #containerElement = null;
+  #moviesModel = null;
+  #moviePopupPresenter = null;
+
+  #movies = [];
+  #sectionComponent = null;
+  #movieCardPresenters = new Map();
+
+  constructor({ containerElement, moviesModel, moviePopupPresenter }) {
+    this.#containerElement = containerElement;
+    this.#moviesModel = moviesModel;
+    this.#moviePopupPresenter = moviePopupPresenter;
+  }
+
+  init() {
+    if (this.#sectionComponent) {
+      this.#destroy();
+    }
+
+    this.#movies = getTopMovies(
+      this.#moviesModel.movies,
+      ({ commentsCount }) => commentsCount, TOP_MOVIES_LIMIT,
+    );
+
+    if (this.#movies.length) {
+      this.#render();
+      this.#moviesModel.addObserver(this.#moviesModelEventHandler);
+    }
+  }
+
+  #renderMovieCard(movie, containerElement) {
+    const movieCardPresenter = new MovieCardPresenter({
+      containerElement: containerElement.element,
+      onLinkClick: this.#movieCardLinkClickHandler,
+      onWatchlistButtonClick: this.#movieWatchlistButtonClickHandler,
+      onWatchedButtonClick: this.#movieWatchedButtonClickHandler,
+      onFavoriteButtonClick: this.#movieFavoriteButtonClickHandler,
+    });
+
+    movieCardPresenter.init(movie);
+    this.#movieCardPresenters.set(movie.id, movieCardPresenter);
+  }
+
+  #render() {
+    this.#sectionComponent = new MostCommentedMoviesView();
+    const movieListComponent = new MovieListView();
+
+    this.#movies.forEach((movie) => {
+      this.#renderMovieCard(movie, movieListComponent);
+    });
+
+    render(movieListComponent, this.#sectionComponent.element);
+    render(this.#sectionComponent, this.#containerElement);
+  }
+
+  #destroy() {
+    remove(this.#sectionComponent);
+    this.#movieCardPresenters.clear();
+  }
+
+  #movieCardLinkClickHandler = (movieId) => {
+    this.#moviePopupPresenter.show(movieId);
+  };
+
+  #movieWatchlistButtonClickHandler = (movieId) => {
+    this.#moviesModel.toggleWatchlistedStatus(movieId);
+  };
+
+  #movieWatchedButtonClickHandler = (movieId) => {
+    this.#moviesModel.toggleWatchedStatus(movieId);
+  };
+
+  #movieFavoriteButtonClickHandler = (movieId) => {
+    this.#moviesModel.toggleFavoritedStatus(movieId);
+  };
+
+  #moviesModelEventHandler = (eventType, data) => {
+    switch (eventType) {
+      case EventType.MOVIE_COMMENTS_UPDATE:
+        this.init();
+        break;
+      default:
+        this.#movieCardPresenters.get(data.id)?.init(data);
+    }
+  };
+}
